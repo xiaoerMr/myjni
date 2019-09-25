da788aa39843376d2090cb2c9cf9dbfb74bf8594
gitee <djingw@sina.cn>
2019-09-25 09:34:06 +0800

    http 框架 修改 apiService 和 HttpManager(继承 SaiHttpManager)有用户实现

---
+++ app/src/main/java/com/sai/myjni/base/ApiService.java
@@ -0,0 +1,29 @@
+package com.sai.myjni.base;
+
+import com.vexcellent.saihttplib.core.ResponseBean;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * @author 店小二
+ * @date 2019/9/19.
+ * GitHub：
+ * email：
+ * description：
+ */
+public interface ApiService {
+
+//    @Headers("")
+
+    @GET("word/word")
+    Observable<String> doLogin1(@Query("num") String num, @Query("page") String page);
+
+
+//    @Headers("User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0")
+
+    @GET("api/qqmusic/84466266/json")
+    Observable<ResponseBean> doLogin();
+
+}
---
+++ app/src/main/java/com/sai/myjni/base/HttpManager.java
@@ -0,0 +1,86 @@
+package com.sai.myjni.base;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.trello.rxlifecycle2.components.support.RxFragmentActivity;
+import com.vexcellent.saihttplib.SaiHttpManager;
+import com.vexcellent.saihttplib.callback.SaiCallBack;
+import com.vexcellent.saihttplib.core.ResponseBean;
+import com.vexcellent.saihttplib.core.SaiHttpFactory;
+import com.vexcellent.saihttplib.core.SaiObserver;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * @author 店小二
+ * @date 2019/9/19.
+ * GitHub：
+ * email：
+ * description：
+ */
+public class HttpManager extends SaiHttpManager {
+
+    private volatile static HttpManager manager;
+    private  ApiService apiService;
+    private static final String BASE_URL = "https://www.sojson.com/";
+
+    private HttpManager() {
+        super();
+    }
+
+
+    public static HttpManager getInstance() {
+        if (null == manager) {
+            synchronized (HttpManager.class) {
+                if (null == manager) {
+                    manager = new HttpManager();
+                }
+            }
+        }
+        return manager;
+    }
+
+//    @Override
+//    public void createApiService() {
+//         apiService = (ApiService) factory.onCreateApiService(ApiService.class, BASE_URL);
+//    }
+
+    public void doLogin(RxAppCompatActivity activity, String num, String page, final SaiCallBack<String> callBack) {
+
+        Observable<ResponseBean> observable = apiService.doLogin();
+        doSubscribe(activity, observable, new SaiObserver<ResponseBean>() {
+            @Override
+            protected void onSuccess(ResponseBean o) {
+                callBack.onSuccess(o.toString());
+            }
+
+            @Override
+            protected void onFailure(Throwable e) {
+                callBack.onError(e.getMessage());
+            }
+        });
+    }
+
+    private void doSubscribe(RxAppCompatActivity activity, Observable<ResponseBean> observable, SaiObserver<ResponseBean> observer){
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+//                .compose(RxLifecycle.bind(activity.lifecycle()))
+                .compose(activity.<ResponseBean>bindToLifecycle())
+                .subscribe(observer);
+    }
+
+    private void doSubscribe(RxFragmentActivity fragmentActivity, Observable<ResponseBean> observable, SaiObserver<ResponseBean> observer) {
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+//                .compose(RxLifecycle.bind(activity.lifecycle()))
+                .compose(fragmentActivity.<ResponseBean>bindToLifecycle())
+                .subscribe(observer);
+    }
+
+
+    @Override
+    public void createApiService() {
+         apiService = (ApiService) factory.onCreateApiService(ApiService.class, BASE_URL);
+    }
+}
